9073f76591128723e14adf90f7db58ee291aad56
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-07-21 15:17:33 -0300

    Second commit

--- js/engine.js
+++ js/engine.js
@@ -1,13 +1,29 @@
 var stateBlender = "off";
 var blender = document.getElementById("blender");
+var blenderSound = document.getElementById("blender-sound");
+var buttonBlender = document.getElementById("blender-button-sound")
 
 
 function blenderControl(){
     if(stateBlender == "off"){
         stateBlender = "on";
-        console.log("turn me on");
+        makeBrrBrr();
+        blender.classList.add("active");
     } else{
         stateBlender = "off"
-        console.log("turn me off");
+        makeBrrBrr();
+        blender.classList.remove("active");
+    }
+}
+
+
+function makeBrrBrr (){
+    if(blenderSound.paused){
+    buttonBlender.play();
+    blenderSound.play();
+    }else{
+        buttonBlender.play();
+        blenderSound.pause();
+        blenderSound.currentTime = 0;
     }
 }
